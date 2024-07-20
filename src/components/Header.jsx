4b8f4179f00d9a35c7cba54987f93930af90f1f9
Alex Mwaura <alecsmwaura@gmail.com>
2024-07-21 00:08:04 +0300

    Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
 import styles from "./header.module.css";
 export default function Header() {
-  return <div className={styles.header}>My Todo List</div>;
+  return <div className={styles.header}>Todo List</div>;
 }
